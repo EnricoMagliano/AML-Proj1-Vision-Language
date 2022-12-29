52d91ec29c89404950f00d3769804682a0564baf
ENRICO MAGLIANO s295692 <s295692@studenti.polito.it>
2022-12-29 15:36:06 +0100

    add print

--- main.py
+++ main.py
@@ -47,11 +47,13 @@ def main(opt):
 
                 if iteration % opt['print_every'] == 0:
                     logging.info(f'[TRAIN - {iteration}] Loss: {total_train_loss / (iteration + 1)}')
+                    print(f'[TRAIN - {iteration}] Loss: {total_train_loss / (iteration + 1)}')
                 
                 if iteration % opt['validate_every'] == 0:
                     # Run validation
                     val_accuracy, val_loss = experiment.validate(validation_loader)
                     logging.info(f'[VAL - {iteration}] Loss: {val_loss} | Accuracy: {(100 * val_accuracy):.2f}')
+                    print(f'[VAL - {iteration}] Loss: {val_loss} | Accuracy: {(100 * val_accuracy):.2f}')
                     if val_accuracy > best_accuracy:
                         experiment.save_checkpoint(f'{opt["output_path"]}/best_checkpoint.pth', iteration, best_accuracy, total_train_loss)
                     experiment.save_checkpoint(f'{opt["output_path"]}/last_checkpoint.pth', iteration, best_accuracy, total_train_loss)
@@ -64,6 +66,7 @@ def main(opt):
     experiment.load_checkpoint(f'{opt["output_path"]}/best_checkpoint.pth')
     test_accuracy, _ = experiment.validate(test_loader)
     logging.info(f'[TEST] Accuracy: {(100 * test_accuracy):.2f}')
+    print(f'[TEST] Accuracy: {(100 * test_accuracy):.2f}')
 
 if __name__ == '__main__':
 
